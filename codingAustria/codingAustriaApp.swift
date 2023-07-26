b75d4def70edf2c9786462e88fcb80ab5efbe0e7
kazbeki <david.pfluegl@gmail.com>
2023-07-26 15:40:18 +0200

    Initial Commit

---
+++ codingAustria/codingAustriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  codingAustriaApp.swift
+//  codingAustria
+//
+//  Created by David Pfluegl on 26.07.23.
+//
+
+import SwiftUI
+
+@main
+struct codingAustriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
